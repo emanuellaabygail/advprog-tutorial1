12bbc98c2f49b9102bfe287e0c64502cac632030
emanuellaabygail <abygailsiahaan1911@gmail.com>
2025-03-06 01:23:22 +0700

    [GREEN] Implement OrderRepository class

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -1,26 +1,25 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
-import id.ac.ui.cs.advprog.eshop.model.Order;
-import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
-
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderRepositoryTest {
     OrderRepository orderRepository;
-    List <Order> orders;
+    List<Order> orders;
 
     @BeforeEach
     void setUp() {
         orderRepository = new OrderRepository();
 
-        List <Product> products = new ArrayList<>();
+        List<Product> products = new ArrayList<>();
         Product product1 = new Product();
         product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
         product1.setProductName("Sampo Cap Bambang");
@@ -30,9 +29,9 @@ class OrderRepositoryTest {
         orders = new ArrayList<>();
         Order order1 = new Order("13652556-012a-4c07-b546-54eb1396d79b", products, 1708560000L, "Safira Sudrajat");
         orders.add(order1);
-        Order order2 = new Order("7f9e15bb-4b15-42f4-aebc-c3af385fb078", products, 1708570000L, "Safira Sudrajat");
+        Order order2 = new Order("7f9e15bb-4b15-42f4-aebc-c3af385fb878", products, 1708570000L, "Safira Sudrajat");
         orders.add(order2);
-        Order order3 = new Order("e334ef40-9eff-4da8-9487-8ee697ecbf1e", products, 1708570000L, "Bambang Sudrajat");
+        Order order3 = new Order("e334ef48-9eff-4da8-9487-8ee697ecbf1e", products, 1708570000L, "Bambang Sudrajat");
         orders.add(order3);
     }
 
@@ -94,7 +93,8 @@ class OrderRepositoryTest {
             orderRepository.save(order);
         }
 
-        List <Order> orderList = orderRepository.findAllByAuthor(orders.get(1).getAuthor());
+        List<Order> orderList = orderRepository.findAllByAuthor(
+                orders.get(1).getAuthor());
         assertEquals(2, orderList.size());
     }
 
@@ -102,7 +102,8 @@ class OrderRepositoryTest {
     void testFindAllByAuthorIfAllLowercase() {
         orderRepository.save(orders.get(1));
 
-        List <Order> orderList = orderRepository.findAllByAuthor(orders.get(1).getAuthor().toLowerCase());
+        List<Order> orderList = orderRepository.findAllByAuthor(
+                orders.get(1).getAuthor().toLowerCase());
         assertTrue(orderList.isEmpty());
     }
 }
