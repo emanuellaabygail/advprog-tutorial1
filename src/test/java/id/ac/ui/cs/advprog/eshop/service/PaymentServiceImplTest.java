85149648680a30dca773590dc27436ea4c76a4e2
emanuellaabygail <abygailsiahaan1911@gmail.com>
2025-03-07 01:25:19 +0700

    [RED] Add PaymentService and PaymentServiceImpl Skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImplTest.java
@@ -68,7 +68,7 @@ public class PaymentServiceImplTest {
         Order order = orders.get(0);
         Payment payment = payments.get(0);
         doReturn(payment).when(paymentRepository).save(any(Payment.class));
-        Payment result = PaymentService.addPayment(order, PaymentMethod.VOUCHER.getValue(), Map.of("voucherCode", "ESHOP1234ABC5678"));
+        Payment result = paymentService.addPayment(order, PaymentMethod.VOUCHER.getValue(), Map.of("voucherCode", "ESHOP1234ABC5678"));
         verify(paymentRepository, times(1)).save(any(Payment.class));
         assertEquals(payment.getId(), result.getId());
     }
